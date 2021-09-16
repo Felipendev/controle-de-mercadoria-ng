fae580e00d73f6cebacef767f1d9808ffda9a1ae
felip <felipe.psic@outlook.com.br>
2021-09-15 22:20:39 -0300

    Atualiza caminhos de componentes

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -2,9 +2,9 @@ import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { BodyComponent } from '../body/body.component';
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
+import { BodyComponent } from '../home/body/body.component';
+import { HeaderComponent } from '../home/header/header.component';
+import { FooterComponent } from '../home/footer/footer.component';
 import { ProdutosListComponent } from './produtos-list/produtos-list.component';
 
 @NgModule({
