62877ebbbf16a168ad61533a362236858483b108
felip <felipe.psic@outlook.com.br>
2021-10-04 16:33:31 -0300

    Muda Cliente form para um unico comp e add menu hamburguer

--- src/app/shared/service/cliente.service.ts
+++ src/app/shared/service/cliente.service.ts
@@ -1,4 +1,3 @@
-import { FiltroPipe } from './../pipe/filtro-form/filtro.pipe';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
--- src/app/views/home/cliente-form/cliente-form.component.ts
+++ src/app/views/home/cliente-form/cliente-form.component.ts
@@ -1,7 +1,10 @@
-import { Cliente } from './../../../shared/model/cliente.model';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment';
+
 import { ClienteService } from 'src/app/shared/service/cliente.service';
+import { StatusProduto } from 'src/app/shared/model/status-produto.enum';
+import { Cliente } from 'src/app/shared/model/cliente.model';
 
 
 
@@ -11,25 +14,56 @@ import { ClienteService } from 'src/app/shared/service/cliente.service';
   styleUrls: ['./cliente-form.component.css']
 })
 export class ClienteFormComponent implements OnInit {
+
+  isShow = false;  
   searchValue!: string;
   form!: FormGroup;
   submitted = false;
   clientes: Cliente[] = [];
-
+  
   
   constructor(
     private fb: FormBuilder,
     private service: ClienteService
-  ) { }
+    ) { }
+    
+    ngOnInit(): void {
+      this.form = this.fb.group({
+        nome: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
+        sobrenome: [null],
+        codigo: [null, [Validators.required]],
+        dataRecebimento: [ null, [Validators.required]],
+        horaRecebimento: [null, [Validators.required]],
+        contato: [null, [Validators.required]],
+        statusProduto: StatusProduto.RECEBIDO
+      });
+      
+      this.getClientes();
+    }
+    
+    getClientes(){
+      this.service.getClientes().subscribe(data => {
+        this.clientes = data;
+      });
+    }
+    
+    criaCliente(){
+      this.submitted = true;
+      let newDate: moment.Moment = moment.utc(this.form.value.dataRecebimento).local();
+      this.form.value.dataRecebimento = newDate.format("YYYY-MM-DD");
+      this.service.postCliente(this.form.value).subscribe(result => {});
+      location.assign("/home");
+    }
+    
+    cancelar(){
+      this.submitted = false;
+      this.form.reset();
+    }
 
-  ngOnInit(): void {
-    this.getClientes();
-  }
 
-  getClientes(){
-    this.service.getClientes().subscribe(data => {
-      this.clientes = data;
-    });
-  }
+    toggle() {
+      this.isShow = !this.isShow;
+    }
+  
 }
 
