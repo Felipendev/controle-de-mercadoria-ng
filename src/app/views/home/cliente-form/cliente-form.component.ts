576e39364b96bb505719c7135e1342ac3a7f8854
felip <felipe.psic@outlook.com.br>
2021-10-04 21:29:07 -0300

    add de validaçao de form

--- src/app/views/home/cliente-form/cliente-form.component.ts
+++ src/app/views/home/cliente-form/cliente-form.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as moment from 'moment';
 
 import { ClienteService } from 'src/app/shared/service/cliente.service';
 import { StatusProduto } from 'src/app/shared/model/status-produto.enum';
 import { Cliente } from 'src/app/shared/model/cliente.model';
-
-
 
 @Component({
   selector: 'app-cliente-form',
@@ -20,24 +18,24 @@ export class ClienteFormComponent implements OnInit {
   form!: FormGroup;
   submitted = false;
   clientes: Cliente[] = [];
-  
+
+  @Input() pattern!: string | RegExp
   
   constructor(
     private fb: FormBuilder,
     private service: ClienteService
     ) { }
-    
+
     ngOnInit(): void {
       this.form = this.fb.group({
         nome: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
-        sobrenome: [null],
-        codigo: [null, [Validators.required]],
+        sobrenome: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
+        codigo: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(4)]],
         dataRecebimento: [ null, [Validators.required]],
         horaRecebimento: [null, [Validators.required]],
         contato: [null, [Validators.required]],
         statusProduto: StatusProduto.RECEBIDO
       });
-      
       this.getClientes();
     }
     
@@ -52,7 +50,7 @@ export class ClienteFormComponent implements OnInit {
       let newDate: moment.Moment = moment.utc(this.form.value.dataRecebimento).local();
       this.form.value.dataRecebimento = newDate.format("YYYY-MM-DD");
       this.service.postCliente(this.form.value).subscribe(result => {});
-      location.assign("/home");
+      // location.assign("/home");
     }
     
     cancelar(){
